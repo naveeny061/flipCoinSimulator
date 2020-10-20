020dfbab26f18550c68a5b165d77b45e06b256b2
naveeny061 <naveeny061@gmail.com>
2020-10-20 17:30:53 +0530

    refactoring and adding winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 echo "Welcome To Flip Coin Simulator Program"
-loop=0
 heads=0
 tails=0
-while [ $loop -lt 10 ]
+while [ 1 ]
 do
 randomValue=$((RANDOM%2))
-if [ $randomValue -eq 0 ]
+if [[ $randomValue -eq 0 ]]
 then
 ((heads++))
 echo "heads"
@@ -14,7 +13,21 @@ else
 ((tails++))
 echo "tails"
 fi
-((loop++))
+if [[ $heads -eq 21 || $tails -eq 21 ]]
+then 
+break
+fi
 done
 echo "total heads= "$heads
 echo "total tails= "$tails
+if [ $heads -eq $tails ]
+then
+echo "tie"
+elif [[ $heads -gt $tails ]]
+then
+value=$((heads-tails))
+echo "heads win by "$value
+else
+value=$((tails-heads))
+echo "tails win by "$value
+fi
