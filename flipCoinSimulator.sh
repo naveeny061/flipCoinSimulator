c2356e3d827620d7222264cd4e6b472de6aec61a
naveeny061 <naveeny061@gmail.com>
2020-10-20 17:14:49 +0530

    calculating total heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
 echo "Welcome To Flip Coin Simulator Program"
+loop=0
+heads=0
+tails=0
+while [ $loop -lt 10 ]
+do
 randomValue=$((RANDOM%2))
 if [ $randomValue -eq 0 ]
-then 
+then
+((heads++))
 echo "heads"
-else 
+else
+((tails++))
 echo "tails"
 fi
+((loop++))
+done
+echo "total heads= "$heads
+echo "total tails= "$tails
